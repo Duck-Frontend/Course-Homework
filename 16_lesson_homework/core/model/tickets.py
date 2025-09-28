be0195661d9035d776881cf0f8db67ad3f04d345
Anton Stadolnik <stadonikanton1@gmail.com>
2025-09-28 16:12:07 +0300

    Вынесение моделей в разные файлы

---
+++ 16_lesson_homework/core/model/tickets.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, ForeignKey, INTEGER, TIMESTAMP
+from sqlalchemy import Enum
+
+from model.events import Events
+
+Base = declarative_base()
+
+
+class Tickers(Base):
+    __tablename__ = "tickets"
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    event = Column(INTEGER, ForeignKey(Events.id, ondelete='CASCADE'))
+    ticket_status = Column(
+        Enum('available', 'booked', 'sold', 'cancelled',
+             name='ticket_status_enum'),
+        nullable=False,
+        default='available'
+    )
+    created_at = Column(TIMESTAMP)
