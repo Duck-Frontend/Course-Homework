74c3cb0d887d86689b406f80fbb6cface2279a3c
Anton Stadolnik <stadonikanton1@gmail.com>
2025-09-28 17:12:21 +0300

    Вынесение логики view на отдельные части

--- 16_lesson_homework/core/view/events.py
+++ 16_lesson_homework/core/view/events.py
@@ -1,9 +1,20 @@
-class View:
-    def show_all_events(self, data):
+import tabulate
+
+from core.controller.events import Events_Controller
+
+from core.view.view import View
+
+
+class Events_View(View):
+    def __init__(self):
+        self.events_controller = Events_Controller()
+
+    def show_all_events(self):
+        data = self.events_controller.select_all_events()
         print(tabulate.tabulate(data))
 
-    def __add_event(self):
-        self.controller.create_event()
+    def add_event(self):
+        self.events_controller.create_event()
 
     def __edit_event(self):
         pass
@@ -11,10 +22,12 @@ class View:
     def __delete_event(self):
         pass
 
-    def __find_event_by_name(self, data):
+    def find_event_by_name(self):
+        data = self.events_controller.select_event_by_name()
         print(tabulate.tabulate(data))
 
-    def __find_events_by_place(self, data):
+    def find_events_by_place(self):
+        data = self.events_controller.select_event_by_place()
         print(tabulate.tabulate(data))
 
     def __searh_events_partial(self):
